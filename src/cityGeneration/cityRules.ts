47c68d5782c6a56cc20c008939fac563dd042757
aroquev00 <aroquev00@users.noreply.github.com>
2022-04-29 10:47:33 -0500

    Refactor cityRules.

--- src/cityGeneration/cityRules.ts
+++ src/cityGeneration/cityRules.ts
@@ -35,41 +35,37 @@ export class RoadRuleSystem {
     }
   }
 
+  getAvailableMovementActions() {
+    return this.movementActionRules.map(rule => rule.action);
+  }
+
   getMovementAction(): string {
+    return this.getActionFromRules(this.movementActionRules);
+  }
+  
+
+  shouldBranchOut(): boolean {
+    return (this.getActionFromRules(this.branchOutRules) === "doBranchOut");
+  }
+
+  shouldCrossIntersectingRoad(): boolean {
+    return (this.getActionFromRules(this.crossIntersectionRules) === "doCrossIntersection");
+  }
+
+  allowDeadEnd(): boolean {
+    return (this.getActionFromRules(this.deadEndRules) === "doAllowDeadEnd");
+  }
+
+  getActionFromRules(rules: RoadRule[]): string {
     const prob = Math.random();
     let accumulatedProb = 0.0;
-    for (let rule of this.movementActionRules) {
+    for (let rule of rules) {
       accumulatedProb += rule.probability;
       if (prob <= accumulatedProb) {
         return rule.action;
       }
     }
-    throw new Error("Rules do not add up to 1.");
+    throw new Error("Rules do not add up to at least 1.");
   }
 
-  shouldBranchOut(): boolean {
-    const prob = Math.random();
-    if (prob > 0.8) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  shouldCrossIntersectingRoad(): boolean {
-    const prob = Math.random();
-    if (prob > 0.5) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  allowDeadEnd(): boolean {
-    return false;
-  }
-
-  getAvailableMovementActions() {
-    return this.movementActionRules.map(rule => rule.action);
-  }
 }
